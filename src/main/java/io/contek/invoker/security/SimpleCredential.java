dd5aafdf15c06fc1869d1804d57a799a5453f34d
kong <kong_k@hotmail.com>
2025-04-21 12:26:52 +0300

    caching Mac after initializing with secret

--- src/main/java/io/contek/invoker/security/SimpleCredential.java
+++ src/main/java/io/contek/invoker/security/SimpleCredential.java
@@ -3,6 +3,7 @@ package io.contek.invoker.security;
 import is.fm.util.Encoder;
 
 import javax.annotation.concurrent.Immutable;
+import javax.crypto.Mac;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -13,11 +14,13 @@ public final class SimpleCredential implements ICredential {
   private final ApiKey apiKey;
   private final SecretKeyAlgorithm algorithm;
   private final Encoder encoding;
+  private final Mac mac;
 
   SimpleCredential(ApiKey apiKey, SecretKeyAlgorithm algorithm, Encoder encoding) {
     this.apiKey = apiKey;
     this.algorithm = algorithm;
     this.encoding = encoding;
+    mac = algorithm.setupMac(apiKey.getSecret());
   }
 
   @Override
@@ -41,7 +44,7 @@ public final class SimpleCredential implements ICredential {
   }
 
   @Override
-  public final String sign(String payload) {
-    return encoding.encode(algorithm.setupMac(apiKey.getSecret()).doFinal(payload.getBytes(UTF_8)));
+  public String sign(String payload) {
+    return encoding.encode(mac.doFinal(payload.getBytes(UTF_8)));
   }
 }
